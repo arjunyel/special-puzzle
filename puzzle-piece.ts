b12b7f864c91bddeb59a5495b8e5b8046b52c947
arjunyel <arjunyel@gmail.com>
2017-03-10 00:34:02 -0600

    Working on display

--- puzzle-piece.ts
+++ puzzle-piece.ts
@@ -2,12 +2,57 @@ const importedDoc = document.currentScript.ownerDocument;
 /*This lines allows the elment to be created by document.createElement()*/
 
 class PuzzlePiece extends HTMLElement {
+
     constructor() {
         super();
-        console.dir(document.currentScript);
         const t = importedDoc.getElementById("piece-puzzle") as HTMLTemplateElement;
         const shadowRoot = this.attachShadow({mode: "open"});
         shadowRoot.appendChild(t.content.cloneNode(true));
+     }
+
+     static get oberservedAttributes() {
+         return ["value", "revealed"];
+     }
+
+     public set value(v: string) {
+         if (v) {
+             this.setAttribute("value", v);
+             if (this.value === " ") {
+                 console.log("found space");
+                 this.shadowRoot.querySelector(".box").className = "space";
+             }
+             this.shadowRoot.querySelector("#out").textContent = this.getAttribute("value");
+         } else {
+             this.removeAttribute("value");
+         }
+     }
+
+     public get value(): string {
+        const val = this.getAttribute("value");
+        if (val) {
+            return val;
+        }
+        return "";
+     }
+
+     public get revealed() {
+         return this.hasAttribute("revealed");
+     }
+
+     public set revealed(val) {
+         if (val) {
+             this.setAttribute("revealed", "");
+         } else {
+             this.removeAttribute("revealed");
+         }
+     }
+
+     private attributeChangedCallback(attr: string, oldValue: string|boolean, newValue: string|boolean) {
+         if (attr === "value" ) {
+             this.value = newValue as string;
+         } else if ((newValue === true || newValue === false) && attr === "revealed") {
+             this.revealed = newValue;
+         }
      }
 };
 customElements.define("puzzle-piece", PuzzlePiece);
--- special-puzzle.ts
+++ special-puzzle.ts
@@ -5,22 +5,22 @@ class SpecialPuzzle extends HTMLElement {
         const t = document.currentScript.ownerDocument.getElementById("container-grid") as HTMLTemplateElement;
         const shadowRoot = this.attachShadow({mode: "open"});
         shadowRoot.appendChild(t.content.cloneNode(true));
+        shadowRoot.addEventListener("click", this.childClick);
      }
 
      private connectedCallback() {
          this.renderGrid();
      }
 
-     /**
-      * renderShadow
-      */
      private renderGrid() {
             if (this.shadowRoot) {
                 const proverb = this.proverb;
+                const shadow = this.shadowRoot.getElementById("container") as HTMLElement;
                 for (let i = 0; i < proverb.length; i++) {
                     const piece = document.createElement("puzzle-piece");
-                    piece.textContent = proverb.charAt(i);
-                    this.shadowRoot.getElementById("container").appendChild(piece);
+                    piece.value = proverb.charAt(i);
+                    piece.revealed = true;
+                    shadow.appendChild(piece);
                 }
             }
      }
@@ -42,5 +42,11 @@ class SpecialPuzzle extends HTMLElement {
          const proverb = list[Math.floor(Math.random() * list.length)];
          return proverb;
      }
+
+     private childClick(e: Event) {
+        e.target.revealed = false;
+     }
+
+
 };
 customElements.define("special-puzzle", SpecialPuzzle);
